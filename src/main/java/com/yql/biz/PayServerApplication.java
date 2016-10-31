304c98047289633b620c8026e715ebb7e0d84b52
zhanglin <zhanglin@yqlpay.com>
2016-10-31 20:39:13 +0800

    init project

---
+++ src/main/java/com/yql/biz/PayServerApplication.java
@@ -0,0 +1,12 @@
+package com.yql.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayServerApplication.class, args);
+	}
+}
