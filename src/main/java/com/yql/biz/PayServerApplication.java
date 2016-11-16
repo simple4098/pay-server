da94f6893edb30675c50abe1614388f7ada40cf9
zhanglin <zhanglin@yqlpay.com>
2016-11-16 11:52:41 +0800

    引入feign 接口调用。

--- src/main/java/com/yql/biz/PayServerApplication.java
+++ src/main/java/com/yql/biz/PayServerApplication.java
@@ -3,6 +3,7 @@ package com.yql.biz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement(mode = AdviceMode.ASPECTJ, proxyTargetClass = true)
 @ServletComponentScan
 @EnableFeignClients
+@EnableDiscoveryClient
 public class PayServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yql/biz/controller/DemoController.java
+++ src/main/java/com/yql/biz/controller/DemoController.java
@@ -1,10 +1,17 @@
 package com.yql.biz.controller;
 
+import com.yql.biz.client.ComputeClient;
 import com.yql.biz.model.Order;
 import com.yql.biz.service.IPayService;
 import com.yql.biz.web.ResponseModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,13 +23,30 @@ import javax.annotation.Resource;
  */
 @RestController
 public class DemoController {
-
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
     @Resource
     private IPayService payService;
+    @Resource
+    private ComputeClient computeClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/message", method = RequestMethod.GET)
     public ResponseModel pongMessage( Integer input) {
         Order pay = payService.pay(input);
         return ResponseModel.SUCCESS(pay);
+    }
+
+    @RequestMapping("/add-feign")
+    public Integer addTest(@RequestParam Integer a,@RequestParam Integer b){
+        return computeClient.add(a,b);
+    }
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
     }
 }
